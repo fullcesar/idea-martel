1e7013dfe03e3353b7525e2278fa5c5d5aef2ca7
= <=>
2021-06-01 18:25:34 -0500

    Desafío: Detalle de Producto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/Navbar";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   return (
       <>
         <NavBar />
         <ItemListContainer />
+        <ItemDetailContainer />
       </>
   );
 }
---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,13 @@
+import 'materialize-css';
+import {Row} from 'react-materialize';
+import ItemDetail from '../components/ItemDetail';
+
+const ItemDetailContainer = () => {
+    return (
+        <>
+            <ItemDetail />
+        </>
+    )
+}
+
+export default ItemDetailContainer
